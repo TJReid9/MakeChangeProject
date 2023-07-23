13bc26eeaff06bb0a8389c954f4807657bff2c4a
TJReid9 <tjreid20@icloud.com>
2023-07-22 20:32:36 -0600

    Completed MakeChange

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -1,10 +1,81 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Price: ");
+		float price = sc.nextFloat();
+
+		System.out.println("Paid: ");
+		float paid = sc.nextFloat();
+		sc.close();
+
+		float totalChange = (paid - price);
+
+		if (paid < price) {
+			System.out.println("Not enough.");
+		} else if (paid == price) {
+			System.out.println("Exact amount.");
+		} else {
+			System.out.printf("Change: %.2f\n", totalChange);
+			
+			int dollars = (int) totalChange;
+			int cents = (int)((totalChange-(float)dollars)*100);
+
+			int twenties = dollars / 20;
+			dollars = dollars % 20;
+
+			int tens = dollars / 10;
+			dollars = dollars % 10;
+
+			int fives = dollars / 5;
+			dollars = dollars % 5;
+
+			int ones = dollars / 1;
+			dollars = dollars % 1;
+
+			int quarters = cents / 25;
+			cents = cents % 25;
+
+			int dimes = cents / 10;
+			cents = cents % 10;
+
+			int nickels = cents / 5;
+			cents = cents % 5;
+
+			int pennies = cents / 1;
+			cents = cents % 1;
+			
+
+			if (twenties > 0) {
+				System.out.println("Twenties: " + twenties);
+			}
+			if (tens > 0) {
+				System.out.println("Tens: " + tens);
+			}
+			if (fives > 0) {
+				System.out.println("Fives: " + fives);
+			}
+			if (ones > 0) {
+				System.out.println("Ones: " + ones);
+			}
+			if (quarters > 0) {
+				System.out.println("Quarters: " + quarters);
+			}
+			if (dimes > 0) {
+				System.out.println("Dimes: " + dimes);
+			}
+			if (nickels > 0) {
+				System.out.println("Nickels: " + nickels);
+			}
+			if (pennies > 0) {
+				System.out.println("Pennies: " + pennies);
+			}
+		}
 
 	}
-
 }
